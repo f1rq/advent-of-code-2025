bdbbf46af13aaa3cd9661d21be34174aa5a4d4f3
f1rq <f.czudaj@tutanota.com>
2025-12-06 22:03:01 +0100

    Day 2, part 1 & 2.

---
+++ day2-part1/day2-part1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace day2_part1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool IsDouble(long id)
+            {
+                string s = id.ToString();
+
+                if (s.Length % 2 != 0)
+                {
+                    return false;
+                }
+
+                int half = s.Length / 2;
+                string left = s.Substring(0, half);
+                string right = s.Substring(half);
+
+                return left == right;
+            }
+            
+            var input = File.ReadLines("input.txt");
+            var pairs = input
+                .SelectMany(line => line.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                .ToArray();
+
+            long total = 0;
+
+            foreach (var pair in pairs)
+            {
+                var nums = pair.Split('-');
+                long start = long.Parse(nums[0]);
+                long end = long.Parse(nums[1]);
+
+                for (long id = start; id <= end; id++)
+                {
+                    if (IsDouble(id))
+                    {
+                        total += id;
+                    }
+                }
+            }
+            
+            Console.WriteLine($"Total: {total}");
+        }
+    }
+}
