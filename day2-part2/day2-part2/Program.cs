471f5b67fef20d7762fe16c038e8f2f9fa798305
f1rq <f.czudaj@tutanota.com>
2025-12-10 19:27:17 +0100

    .

---
+++ day2-part2/day2-part2/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace day2_part2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool IsDouble(long id)
+            {
+                string s = id.ToString();
+                int len = s.Length;
+
+                for (var blockLength = 1; blockLength <= len / 2; blockLength++)
+                {
+                    if (len % blockLength != 0)
+                    {
+                        continue;
+                    }
+
+                    string block = s.Substring(0, blockLength);
+                    int repeat = len / blockLength;
+
+                    string newBlock = string.Join("", Enumerable.Repeat(block, repeat));
+
+                    if (newBlock == s)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            
+            var input = File.ReadLines("input.txt");
+            var pairs = input
+                .SelectMany(line => line.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                .ToArray();
+
+            long total = 0;
+
+            foreach (var pair in pairs)
+            {
+                var nums = pair.Split('-');
+                long start = long.Parse(nums[0]);
+                long end = long.Parse(nums[1]);
+
+                for (long id = start; id <= end; id++)
+                {
+                    if (IsDouble(id))
+                    {
+                        total += id;
+                    }
+                }
+            }
+            
+            Console.WriteLine($"Total: {total}");
+        }
+    }
+}
